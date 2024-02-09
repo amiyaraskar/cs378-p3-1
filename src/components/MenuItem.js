c5a9da87b41268c35ba1625b97b278d64bad65f4
Amiya <amiyaraskar@gmail.com>
2024-02-09 16:57:41 -0600

    changed parameters

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -5,7 +5,7 @@ import React from 'react';
 // Use bootstrap to style the elements so that it looks like the mockup in the assignment.
 // Hint: You can use the image name to get the image from the images folder.
 
-const MenuItem = ({ title, description, imageName, price }) => {
+const MenuItem = ({ imageName, title, description, price }) => {
     return (
         <div className="container">
             <div className="row">
