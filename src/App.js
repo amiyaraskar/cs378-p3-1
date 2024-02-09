c59353187a7d8d4c9640de06ec5f6fec9460c780
Amiya <amiyaraskar@gmail.com>
2024-02-09 15:58:45 -0600

    restart base

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import MenuItem from './components/MenuItem';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import Header from './components/Header';
+// This imports bootstrap css styles. You can use bootstrap or your own classes by using the className attribute in your elements.
 
-// import 'bootstrap/dist/css/bootstrap.min.css'; // This imports bootstrap css styles. You can use bootstrap or your own classes by using the className attribute in your elements.
-
-// Menu data. An array of objects where each object represents a menu item. Each menu item has an id, title, description, image name, and price.
+// Menu data. An array of objects where each object represents a menu item.
+// Each menu item has an id, title, description, image name, and price.
 // You can use the image name to get the image from the images folder.
+
 const menuItems = [
   {
     id: 1,
@@ -78,14 +81,35 @@ const menuItems = [
   }
 ];
 
+const menuHeader = [
+  {
+    imageName: 'logo.png',
+    subheading1: 'Delicious Japanese Cuisine',
+    subheading2: 'The Fresh Choice of UT!'
+  }
+]
 
 function App() {
   return (
     <div>
-      <h1>Menu</h1>
+      {menuHeader.map((header) => (
+        <Header
+          imageName={header.imageName}
+          subheading1={header.subheading1}
+          subheading2={header.subheading2}
+        />
+      ))}
       <div className="menu">
         {/* Display menu items dynamicaly here by iterating over the provided menuItems */}
-        <MenuItem title={menuItems[0].title} /> {/* Example for how to use a component */}
+        {menuItems.map((menuItem) => (
+          <MenuItem
+            key={menuItem.id}
+            title={menuItem.title}
+            description={menuItem.description}
+            imageName={menuItem.imageName}
+            price={menuItem.price}
+          />
+        ))}
       </div>
     </div>
   );
--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -1,14 +1,35 @@
 import React from 'react';
 
-
 // This is a functional component that represents a single menu item. It currently takes in the title and displays it in an h2 element.
 // Modify the component to take in all the other properties of a menu item you need and display them in the component.
 // Use bootstrap to style the elements so that it looks like the mockup in the assignment.
 // Hint: You can use the image name to get the image from the images folder.
-const MenuItem = ({ title }) => {
+
+const MenuItem = ({ title, description, imageName, price }) => {
     return (
-        <div>
-            <h2>{title}</h2>
+        <div className="container">
+            <div className="row">
+                <div className="col-4">
+                    <img 
+                        src={require(`../images/${imageName}`)} 
+                        alt={title} 
+                        className="rounded-3" 
+                        style={{ width: "110%" }}
+                    />
+                </div>
+                <div className="col-8">
+                    <div className="card-body">
+                        <h5 className="card-title">{title}</h5>
+                        <p className="card-text">{description}</p>
+                    </div>
+                    <div className="card-footer d-flex justify-content-between align-items-center">
+                        <p className="card-text-price">Price: ${price}</p>
+                        <button className="rounded-3">
+                            Add
+                        </button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
